c0273e0ffd0fed2c0a56c848b44f44bb78c0df41
Kai Hotz <kaihotz@me.com>
2018-01-22 08:48:26 +0100

    Added uniqueArrya and Array.from methodes

--- customES6Functions.js
+++ customES6Functions.js
@@ -11,7 +11,10 @@ const balancedParens = string => {
 
 balancedParens('((()))')
 
+
 // removes repeated items from array
+const numbers = [1, 1, 2, 3, 4, 4]
+
 const unique = array => {
   return array.reduce((acc, element) => {
     if (!acc.includes(element)) {
@@ -21,14 +24,22 @@ const unique = array => {
   }, [])
 }
 
-const numbers = [1, 1, 2, 3, 4, 4]
+// or 
+
+const uniqueArray = arr => [...new Set(arr)]
+
 unique(numbers)
+// [1, 2, 3, 4]
+uniqueArray(numbers)
+// [1, 2, 3, 4]
+
 
 // Calculates the fibonacci number given a regular number
 const fibonacci = n => {
   if (n < 3) return 1
   return fibonacci(n - 1) + fibonacci(n - 2)
 }
+
 
 // given an array of arrays returns an array of objects
 const classes = [
@@ -41,8 +52,19 @@ const classesAsObject = classes.map(([subject, time, teacher]) => {
   return {subject, time, teacher}
 })
 
+
 // reorgaize array of objects into object with element.key element.value pair
 const formatObj = (arr) => arr.reduce((acc, element) => {
   acc[element.key] = element.value;
   return acc;
 }, {});
+
+
+// Array.from() accepts a second argument that's a `map` function. Useful for calling on each element of an array you created.
+const year = new Date().getFullYears()
+const totalYears = 5
+
+Array.from(Array(totalYears), (..., i) => year +i)
+// [2018, 2019, 2020, 2022]
+
+Array.from({length: totalYears}, (..., i) => year +i)
