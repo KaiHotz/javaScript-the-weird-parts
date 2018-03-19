b85ae8f0a5c7a0e43664da4b17e24f8f4aa33c48
Kai Hotz <kaihotz@me.com>
2018-03-19 20:36:46 +0100

    - added moonfare challange

---
+++ moonfare_challenge.js
@@ -0,0 +1,44 @@
+
+// You've built an inflight entertainment system with on-demand movie streaming.
+
+// Users on longer flights like to start a second movie right when their first one ends, but they complain that the plane usually lands before they can see the ending. So you're building a feature for choosing two movies whose total runtimes will equal the exact flight length.
+
+// Write a function that takes an integer flight_length (in minutes) and a list of integers movie_lengths (in minutes) and returns a boolean indicating whether there are two numbers in movie_lengths whose sum equals flight_length.
+
+// When building your function:
+//   Assume your users will watch exactly two movies
+//   Don't make your users watch the same movie twice
+//   Optimize for runtime over memory
+
+const flight_length = 210
+
+const movie_lengths = [120, 90, 30]
+
+const getMoviesForFlight = (movie_lengths, flight_length) => {
+  let result = []
+  const movies = movie_lengths.reduce((acc, el) => {
+    let sum = acc + el
+    if (el < flight_length && sum <= flight_length) {
+      result.push(el)
+    }
+    return result
+  }, 0)
+
+  return movies
+}
+
+console.log(getMoviesForFlight(movie_lengths, flight_length))
+
+const getMoviesForFlight_2 = (movie_lengths, flight_length) => {
+  let result = []
+  for (let i = 0; i < movie_lengths.length; i += 1) {
+    for (let j = i + 1; j < movie_lengths.length; j += 1) {
+      if (movie_lengths[j] === flight_length - movie_lengths[i]) {
+        result.push(movie_lengths[i], movie_lengths[j])
+      }
+    }
+  }
+  return result
+}
+
+console.log(getMoviesForFlight_2(movie_lengths, flight_length))
